07ade3b97d81d0dbfd9bdb0c4a2217dc5575e022
mohammed dhedy <mhmddhedy1234@gmail.com>
2025-03-29 03:39:54 +0200

    add the theaters for the shows , make a max width for the showtime cards and add the rating to each card

--- components/showTimesContainer.tsx
+++ components/showTimesContainer.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { Showtime } from "@/types/types";
-// import { getMovieById } from "@/lib/movie-data";
 import React from "react";
-// import { useState, useEffect } from "react";
 import ShowtimeItem from "./showtimeItem";
 import {movies} from "@/data/movies";
 interface Iprops {
@@ -10,11 +8,7 @@ interface Iprops {
 }
 
 const ShowTimesContainer = ({ moviesShowTimes }: Iprops) => {
-  // const [showsList, setShowsList] = useState([]);
-  // useEffect(() => {
-   
-  // }, [moviesShowTimes]);
-  return <div className="flex flex-col gap-8 mb-8">{
+  return <div className="flex flex-col gap-8 mb-8 items-start">{
     movies.map((m)=>{
       const MovieshowTimes= moviesShowTimes.filter((showtime) => showtime.movieId === m.id);
       if(MovieshowTimes.length === 0) return null;
--- components/showtimeItem.tsx
+++ components/showtimeItem.tsx
@@ -1,7 +1,7 @@
 import { Movie, Showtime } from "@/types/types";
+import { screens } from "@/data/screens";
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 interface Iprops {
   showTimes: Showtime[];
   movie: Movie;
@@ -9,7 +9,7 @@ interface Iprops {
 
 const ShowtimeItem = ({ showTimes, movie }: Iprops) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg w-full mx-auto flex flex-col items-start border border-gray-300 md:flex-row overflow-hidden">
+    <div className="bg-white shadow-lg rounded-lg w-full mx-auto max-w-6xl flex flex-col items-start border border-gray-300 md:flex-row overflow-hidden">
       <img
         src={movie.image || ""}
         alt={`${movie.title} poster`}
@@ -27,22 +27,27 @@ const ShowtimeItem = ({ showTimes, movie }: Iprops) => {
             Available Showtimes:
           </span>
           <div className="mt-2 flex gap-2 flex-wrap">
-            {showTimes.map((show, index) => (
-              <Link
-                key={index}
-                href={{ pathname: "/booking", query: { showtime: show.id } }}
-                className="text-black bg-white border border-gray-300 cursor-pointer text-[14px] rounded-[7px] flex flex-col px-4 justify-center items-center hover:bg-[#f4f4f5] transition duration-300 ease-in-out"
-              >
-                {show.time}
-                <span className="text-xs text-gray-500">Theater 1</span>
-              </Link>
-            ))}
+            {showTimes.map((show, index) => {
+              const theater = screens.find(
+                (screen) => screen.id === show.screenId
+              );
+              return (
+                <Link
+                  key={index}
+                  href={{ pathname: "/booking", query: { showtime: show.id } }}
+                  className="text-black bg-white border border-gray-300 cursor-pointer text-[14px] rounded-[7px] flex flex-col px-4 justify-center items-center hover:bg-[#f4f4f5] transition duration-300 ease-in-out"
+                >
+                  {show.time}
+                  <span className="text-xs text-gray-500">{theater?.name}</span>
+                </Link>
+              );
+            })}
           </div>
         </div>
 
         <div className="mt-4 absolute top-4 right-4">
           <span className="px-3 py-1 bg-red-500 text-white text-xs font-bold rounded-full hover:opacity-75 transition duration-300 ease-in-out">
-            PG-13
+            {movie.rating}⭐
           </span>
         </div>
       </div>
