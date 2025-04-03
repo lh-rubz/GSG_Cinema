f288e72577a5b3fa7a435943a4f45be6dbc86350
lh-rubz <habuelrub@gmail.com>
2025-04-04 00:31:04 +0300

    made the id not promotion code

--- components/promotionsContainer.tsx
+++ components/promotionsContainer.tsx
@@ -7,7 +7,7 @@ interface Iprops {
 const PromotionsContainer = ({ promotions }: Iprops) => {
   return (
     <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-4 gap-3 sm:gap-4 md:gap-5 lg:gap-6">  {promotions.map((prom) => (
-        <PromotionCard key={prom.discount_code} promotion={prom} />
+        <PromotionCard key={prom.id} promotion={prom} />
       ))}
     </div>
   );
